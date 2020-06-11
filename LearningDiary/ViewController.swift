bbe11a24a1064a0af47a114ed5a2880e75075b84
Dmitriy Danilin <danilindn@icloud.com>
2020-06-11 20:27:38 +0300

    Добавил элементы интерфейса в код

--- LearningDiary/ViewController.swift
+++ LearningDiary/ViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fiveTF: UITextField!
+    @IBOutlet weak var fourTF: UITextField!
+    @IBOutlet weak var threeTF: UITextField!
+    @IBOutlet weak var twoTF: UITextField!
+    
+    @IBOutlet weak var resoultLabel: UILabel!
+    
+    @IBAction func calculationButton(_ sender: UIButton) {
+    }
+    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
 
 
 }
