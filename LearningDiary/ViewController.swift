8f104d85997f87dd5a9db1ac68863ee17ca93908
Dmitriy Danilin <danilindn@icloud.com>
2020-06-11 22:50:26 +0300

    По моему дикие костыли, но оно работает. Позже доработаю
    На заметку: После каждого урока заниматься своей программой, иначе я просто забываю все реализации кода.

--- LearningDiary/ViewController.swift
+++ LearningDiary/ViewController.swift
@@ -18,6 +18,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var resoultLabel: UILabel!
     
     @IBAction func calculationButton(_ sender: UIButton) {
+        
+        let five = Int(fiveTF.text ?? "")
+        let fiveArray = Array(repeating: 5, count: five ?? 0)
+        let four = Int(fourTF.text ?? "")
+        let fourArray = Array(repeating: 4, count: four ?? 0)
+        let three = Int(threeTF.text ?? "")
+        let threeArray = Array(repeating: 3, count: three ?? 0)
+        let two = Int(twoTF.text ?? "")
+        let twoArray = Array(repeating: 2, count: two ?? 0)
+        
+        var arrayMark = [Int]()
+        arrayMark.append(contentsOf: fiveArray)
+        arrayMark.append(contentsOf: fourArray)
+        arrayMark.append(contentsOf: threeArray)
+        arrayMark.append(contentsOf: twoArray)
+        
+        var mark = 0.00
+        for i in arrayMark {
+            mark += Double(i)
+        }
+        
+        let totalMark = (mark / Double(arrayMark.count))
+        
+        resoultLabel.text = NSString(format: "%.2f", totalMark) as String
+        print(arrayMark)
     }
     
 
